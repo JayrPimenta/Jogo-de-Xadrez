7de9188627837c22ecff2dbd4d4c4b9bfac98e06
Nilson Silva <nilson.silva@provider-it.com.br>
2020-04-21 19:59:36 -0300

    Implementacao de teste unitario com junit 5

---
+++ Jogo-de-Xadrez/src/test/java/tabuleiro/TabuleiroTest.java
@@ -0,0 +1,42 @@
+package tabuleiro;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+import xadrez.Cor;
+import xadrez.unidades.Torre;
+
+public class TabuleiroTest {
+
+	
+	@Test
+	@Order(2)
+	public void deveriaLancarExceptionPosicaoNaoExiste() {
+		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+		Unidade unidade = new Torre(tabuleiro, Cor.BRANCO);
+		Posicao posicao = new Posicao(8, 1);
+		
+		Exception exception = assertThrows(TabuleiroExcecoes.class, () ->
+		
+			tabuleiro.colocarUnidade(unidade, posicao));
+		
+		assertEquals("Posição no tabuleiro não existe", exception.getMessage());
+	}
+	
+	@Test
+	@Order(1)
+	public void deveriaColocarUnidade() {
+		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+		Posicao posicao = new Posicao(7, 1);
+		
+		tabuleiro.colocarUnidade(new Torre(tabuleiro, Cor.BRANCO), posicao);
+		
+		Unidade unidade = tabuleiro.unidade(posicao);
+		
+		assertEquals("Tr", unidade.toString());
+	}
+}
